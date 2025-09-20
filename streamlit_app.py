844301cf2685d549e6e56fd4d7cb15778a4275b6
Bennet71 <bennetmoatshe9@gmail.com>
2025-09-21 01:21:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ with st.sidebar:
     st.header("Patient health data")
     famhist = st.selectbox('**Family history**', ('Present', 'Absent'))
     sbp = st.slider('**Systolic blood pressure**', 100, 250, 175)
+    tobacco = st.slider('**cumulative tobacco(kg)', 0, 32.20, 16.1)
     
     
 
