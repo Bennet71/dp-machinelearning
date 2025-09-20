aaef4f0bd394ec094fbd114c2eebdddd361e2f3b
Bennet71 <bennetmoatshe9@gmail.com>
2025-09-20 22:46:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('Heart Disease Predictor')
 
 st.info('This is a predictive AI application')
 
-df = pd.read_csv(/kaggle/input/south-african-heart-disease-dataset/SAHeart.csv'')
+df = pd.read_csv('/kaggle/input/south-african-heart-disease-dataset/SAHeart.csv')
 df
