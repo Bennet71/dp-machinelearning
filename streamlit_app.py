9b2e7256824493252583d4db60d21ee498d9a738
Bennet71 <bennetmoatshe9@gmail.com>
2025-09-21 00:27:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ with st.expander("Data Visualization"):
 #User input
 with st.sidebar:
     st.header("Patient health data")
-    Family_history = st.selectbox('0', '1')
+    famhist = st.selectbox('Family history', ('Present', 'Absent'))
     
 
