c3857ffefdc2bc8198bebbb1e8b1696609aaf4ef
Bennet71 <bennetmoatshe9@gmail.com>
2025-09-21 22:39:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,6 +59,9 @@ target_mapper = {'1': 1,
 def target_encode(val):
   return target_mapper[val]
 
+y = Y_raw.apply(target_encode)
+y
+
  
 with st.expander('input features'):
   st.write('**Input Features**')
